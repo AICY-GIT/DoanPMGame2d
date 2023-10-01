a683096c93eab69b6810ed42c1644c96235fd4c6
AICY-GIT <143994513+AICY-GIT@users.noreply.github.com>
2023-10-01 16:29:54 +0700

    project file

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    public Vector3 moveInput;
+    public Animator animator;
+
+    public void Start( )
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update( )
+    {
+        moveInput.x = Input.GetAxis ("Horizontal");
+        moveInput.y = Input.GetAxis ("Vertical");
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        animator.SetFloat ("Speed", moveInput.sqrMagnitude);
+
+        if (moveInput.x != 0) 
+        {
+            if (moveInput.x > 0)
+                transform.localScale = new Vector3 (1, 1, 0);
+            else
+                transform.localScale = new Vector3 (-1, 1, 0);
+        }
+    }
+}
