91b106f06bc736a2ccd7819f6995b5a5da234089
TranThanhKhang <khangkt666@gmail.com>
2023-11-14 14:58:15 +0700

    DT-29 Hotfix for DT_25

--- Assets/Scripts/ChucNangMenu.cs
+++ Assets/Scripts/ChucNangMenu.cs
@@ -4,12 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class ChucNangMenu : MonoBehaviour
 {
+    public string nameNewGame= "Newest Scenes";
+    public string nameSetting= "Setting";
     public void NewGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(nameNewGame);
     }
     public void QuitGame()
     {
-
+        Application.Quit();
+    }
+    public void OpenSetting()
+    {
+        SceneManager.LoadScene(nameSetting);
     }
 }
